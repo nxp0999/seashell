602a65fafb2098d5575e235e2fb34d4a082830fd
Annie Johnson <anniejohnsonp@gmail.com>
2024-09-18 17:30:19 -0500

    updated code

--- seashell.c
+++ seashell.c
@@ -4,22 +4,17 @@
 #include<unistd.h>
 #include<fcntl.h>
 
-/*#define exit 1
-#define cd 2
-#define path 3
+char** env_path = NULL;
+int env_path_size = 0;
 
-
-typedef struct { char *cmd; int val; } t_symstruct;
-
-static t_symstruct lookuptable[] = {
-    { "exit", exit }, { "cd", cd }, { "path", path }
-    };*/
 
 void internal_exec(char** args);
 void exit_exec();
 void cd_exec(char** args);
-void path_exec();
+void path_exec(char** args);
 char** getArgs(char* line);
+int parallel_cmd_check(char* line);
+void parallel_exec(char* line);
 
 int main(int argc, char* argv[]){
 	
@@ -32,20 +27,24 @@ int main(int argc, char* argv[]){
 		while(1){
 		
 			printf("\ndash>> ");
-			getline(&line, &allocated, stdin);
+			//getline(&line, &allocated, stdin);
+			if (getline(&line, &allocated, stdin) == -1) {
+                		perror("Error reading input");
+			}
+
 			line[strcspn(line, "\n")] = 0;
-
-			char** args = getArgs(line);
-        		int i=0;
-			//while(args[i]!=NULL){
-			//	printf("\n %d : %s", i, args[i]);
-			//	i++;
-			//}
 			
-			internal_exec(args);
-
+			int ps = parallel_cmd_check(line);
+			
+			if(ps == 0){
+				//printf("Inside ps check in main");
+				parallel_exec(line);
+			}
+			else{
+				char** args = getArgs(line);
+				internal_exec(args);
+			}
 		}
-
 	}
 
 	else if (argc == 2){
@@ -55,24 +54,64 @@ int main(int argc, char* argv[]){
 		size_t allocated = 0;
 		FILE *batch_file; 
 		batch_file = fopen(argv[1], "r");
+		
+		if(batch_file == NULL){
+			perror("Error reading file input");
+		}
 
 		while(getline(&line, &allocated, batch_file) != -1){
 			printf("%s", line);
-			//internal_exec(cmd);
 		
 			line[strcspn(line, "\n")] = 0;
 
-			char** args = getArgs(line);
-			internal_exec(args);
+			int ps = parallel_cmd_check(line);
+
+                        if(ps == 0){
+                                parallel_exec(line);
+                        }
+                        else{
+                                char** args = getArgs(line);
+                                internal_exec(args);
+                        }
 		}
 
-		fclose(batch_file);
-		
-
+		fclose(batch_file);	
 	}
 	
 	
 	return 0;
+}
+
+int parallel_cmd_check(char* line){
+
+	if (strstr(line,"&")!=NULL){
+               	return 0;
+        }
+        return -1;
+}
+
+void parallel_exec(char* line){
+
+        char* token;
+        char* saveptr;
+        const char delimiter[] = "&";
+        token = strtok_r(line, delimiter, &saveptr);
+
+        while(token!=NULL){
+                
+		char** args = getArgs(token);
+		internal_exec(args);		
+	
+	/*printf("Arguments:\n");
+	
+	int i = 0;
+	while (args[i] != NULL) {
+	printf("Arg[%d]: %s\n", i, args[i]);  // Print each arg
+		i++;
+	}*/
+        
+		token = strtok_r(NULL, delimiter, &saveptr);
+        }
 }
 
 void internal_exec(char** args){
@@ -87,16 +126,22 @@ void internal_exec(char** args){
 	}
 
 	if (strcmp(args[0], "path")==0){
-		path_exec();
+		path_exec(args);
 		return;
 	}
 	
 	int rc = fork();
-	 
-	if( rc == 0 ){
+	
+	if (rc < 0) {
+        	perror("Fork failed");
+	}
+	else if( rc == 0 ){
 		execvp(args[0], args);
 	}else{
 	        int wc = wait(NULL);
+		if( wc<0 ){
+			perror("Wait failed");
+		}
 	}
 }
 
@@ -109,8 +154,11 @@ void cd_exec(char** args){
 	chdir(args[1]);	
 }
 
-void path_exec(){
-	//HOW
+void path_exec(char** args){
+	
+	if(args[1] == NULL){
+		return;
+	}
 	printf("In path");
 }
 
